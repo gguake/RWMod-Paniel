7b10092b3a50d22f6692d5a4cf6fa236aae7080b
gguake <gguak2@gmail.com>
2021-09-15 10:42:12 +0900

    add cryptosleep

---
+++ Source/AutomataRace/HediffComps/HediffComp_WithAbility.cs
@@ -0,0 +1,32 @@
+﻿//using RimWorld;
+//using Verse;
+
+//namespace AutomataRace
+//{
+//    public class HediffCompProperties_WithAbility : HediffCompProperties
+//    {
+//        public AbilityDef abilityDef;
+        
+//        public HediffCompProperties_WithAbility()
+//        {
+//            compClass = typeof(HediffComp_WithAbility);
+//        }
+//    }
+
+//    public class HediffComp_WithAbility : HediffComp
+//    {
+//        private HediffCompProperties_WithAbility Props => (HediffCompProperties_WithAbility)props;
+
+//        public override void CompPostPostAdd(DamageInfo? dinfo)
+//        {
+//            Log.Message("post added");
+//            parent.pawn.abilities.GainAbility(Props.abilityDef);
+//        }
+
+//        public override void CompPostPostRemoved()
+//        {
+//            Log.Message("post removed");
+//            parent.pawn.abilities.RemoveAbility(Props.abilityDef);
+//        }
+//    }
+//}
---
+++ Source/AutomataRace/RimWorld/Building_AutomatonCryptosleepCasket.cs
@@ -0,0 +1,45 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+using Verse.AI;
+
+namespace AutomataRace
+{
+    public class Building_AutomatonCryptosleepCasket : Building_CryptosleepCasket
+	{
+		//static readonly Func<Building_Casket, Pawn, IEnumerable<FloatMenuOption>> buildingCasket_GetFloatMenuOptions;
+		//static Building_AutomatonCryptosleepCasket()
+		//{
+		//	var dynMethod = new DynamicMethod(
+		//		"GetFloatMenuOptions_Building_Casket", 
+		//		typeof(IEnumerable<FloatMenuOption>), 
+		//		new Type[] { typeof(Building_Casket), typeof(Pawn) }, 
+		//		typeof(Building_Casket));
+
+		//	ILGenerator il = dynMethod.GetILGenerator();
+		//	il.Emit(OpCodes.Ldarg, 0);
+		//	il.Emit(OpCodes.Ldarg, 1);
+		//	il.EmitCall(OpCodes.Call, typeof(Building_Casket).GetMethod("GetFloatMenuOptions", BindingFlags.Public | BindingFlags.Instance), null);
+		//	il.Emit(OpCodes.Ret);
+
+		//	buildingCasket_GetFloatMenuOptions = (Func<Building_Casket, Pawn, IEnumerable<FloatMenuOption>>)dynMethod.CreateDelegate(typeof(Func<Building_Casket, Pawn, IEnumerable<FloatMenuOption>>));
+		//}
+
+		public override IEnumerable<FloatMenuOption> GetFloatMenuOptions(Pawn myPawn)
+		{
+			if (myPawn.def != AutomataRaceDefOf.Paniel_Race)
+			{
+				yield return new FloatMenuOption("CannotUseReason".Translate("PN_AUTOMATACRYPTOSLEEPONLY".Translate()), null);
+				yield break;
+			}
+
+			foreach (var option in base.GetFloatMenuOptions(myPawn))
+            {
+				yield return option;
+            }
+		}
+    }
+}
